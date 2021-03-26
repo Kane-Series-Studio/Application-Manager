396f83efc15ac89997acf091b965c1eabb5137f1
n0 <74681675+LouisTheXIV@users.noreply.github.com>
2021-03-26 20:41:27 +0200

    Update hardwareInfo.py
    Added a lot of other useful variables

--- backend/hardwareInfo.py
+++ backend/hardwareInfo.py
@@ -1,7 +1,10 @@
 import platform
 import cpuinfo
 import psutil
+from psutil import virtual_memory
 import wmi
+from tabulate import tabulate
+import GPUtil
 
 runningProcessesF = wmi.WMI()
 
@@ -32,18 +35,76 @@ class taskmanager:
         for process in runningProcessesF.Win32_Process():
             self.runningProcesses.append(f"{process.Name}")
         return self.runningProcesses
+
+
+def getDisk(what):
+    totalmem = 0
+    freemem = 0
+    usedmem = 0
+
+    partitions = psutil.disk_partitions()
+    for partition in partitions:
+        try:
+            partition_usage = psutil.disk_usage(partition.mountpoint)
+        except PermissionError:
+            continue
+        totalmemstr = int(''.join([i for i in get_size(partition_usage.total)[:-4] if i.isdigit()]))
+        freememstr = int(''.join([i for i in get_size(partition_usage.free)[:-4] if i.isdigit()]))
+        usedmemstr = int(''.join([i for i in get_size(partition_usage.used)[:-4] if i.isdigit()]))
+        totalmem += totalmemstr
+        freemem += freememstr
+        usedmem += usedmemstr
+    
+    if what == "used":
+        return usedmem
+    elif what == "free":
+        return freemem
+    elif what == "total":
+        return totalmem
+
+ 
+
+    
+
+def get_size(bytes, suffix="B"):
+    factor = 1024
+    for unit in ["", "K", "M", "G", "T", "P"]:
+        if bytes < factor:
+            return f"{bytes:.2f}{unit}{suffix}"
+        bytes /= factor
         
 
 
 tm = taskmanager()
+svmem = psutil.virtual_memory()
+
 #### all variables
+
+processor
+bits
+brand
+hz
+cpuVersion
+processes
+TotalRAM
+AvailableRam
+UsedRAM
+PercentageRAM
+TotalMemory
+FreeMemory
+UsedMemory
+
 processor = tm.getProcessor()
 bits = tm.getBits()
 brand = tm.getBrand()
 hz = tm.getHZ()
 cpuVersion = tm.getCpuVersion()
 processes = tm.getProcesses()
-
-
-
+TotalRAM = get_size(svmem.total)
+AvailableRam = get_size(svmem.available)
+UsedRAM = get_size(svmem.used)
+PercentageRAM = get_size(svmem.percent)
+TotalMemory = getDisk("total")
+FreeMemory = getDisk("free")
+UsedMemory = getDisk("used")
 
