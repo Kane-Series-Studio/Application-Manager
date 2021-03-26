850af57311d6ee5a5faa738318c1af4e4757d24e
n0 <74681675+LouisTheXIV@users.noreply.github.com>
2021-03-26 19:15:21 +0200

    Create hardwareInfo.py

---
+++ backend/hardwareInfo.py
@@ -0,0 +1,37 @@
+import platform
+import cpuinfo
+import psutil
+import wmi
+
+runningProcessesF = wmi.WMI()
+
+class taskmanager:
+    
+    def getProcessor(self):
+        self.yourProcessor = platform.processor()
+        return self.yourProcessor
+
+    def getBits(self):
+        self.yourBits = cpuinfo.get_cpu_info()['bits']
+        return self.yourBits
+
+    def getBrand(self):
+        self.yourBrand = cpuinfo.get_cpu_info()['brand_raw']
+        return self.yourBrand
+
+    def getHZ(self):
+        self.yourHZ = cpuinfo.get_cpu_info()['hz_actual']
+        return self.yourHZ
+
+    def getCpuVersion(self):
+        self.yourCpVersion = cpuinfo.get_cpu_info()['cpuinfo_version_string']
+        return self.yourCpuVersion
+
+    def getProcesses(self):
+        self.runningProcesses = []
+        for process in runningProcessesF.Win32_Process():
+            self.runningProcesses.append(f"{process.Name}")
+        return self.runningProcesses
+        
+
+tm = taskmanager()
